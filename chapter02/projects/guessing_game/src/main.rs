fdd0f54579e913dd0b92fd176e3519fd2e544aa7
livrth <lyvrth@gmail.com>
2021-03-03 12:28:01 +0800

    Update ch2

--- chapter02/projects/guessing_game/src/main.rs
+++ chapter02/projects/guessing_game/src/main.rs
@@ -1,23 +1,24 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-fn main(){
+fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1,101);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     //println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input you guess.");
 
-        let mut guess = String::new(); 
+        let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse(){
+        let guess: u32 = match guess.trim().parse() {
             //.expect("Please type a number!");
             Ok(num) => num,
             Err(_) => continue,
@@ -25,21 +26,13 @@ fn main(){
 
         println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number){
+        match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal =>{
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
     }
 }
-
-
-
-
-
-
-
-
